834104576d7941d156d3d055b48230dc8047c143
dxprx <87080137+dxprx@users.noreply.github.com>
2022-08-04 16:51:12 +0000

    Add files via upload

--- src/js/scrollAnimation.js
+++ src/js/scrollAnimation.js
@@ -1,3 +1,6 @@
+
+
+
 function reveal() {
     var reveals = document.querySelectorAll(".reveal");
   
@@ -5,13 +8,22 @@ function reveal() {
       var windowHeight = window.innerHeight;
       var elementTop = reveals[i].getBoundingClientRect().top;
       var elementVisible = 150;
-  
+    //   console.log(elementTop + " elementoTop");
       if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
+          reveals[i].classList.add("active");
+        
       } else {
         reveals[i].classList.remove("active");
       }
+
     }
+
+    // console.log(reveals);
+    // console.log(windowHeight);
+    // console.log(elementVisible);
   }
-  
+
+
   window.addEventListener("scroll", reveal);
+
+ 
--- src/php/ladder.php
+++ src/php/ladder.php
@@ -1,6 +1,24 @@
 <?php
 require "../config/config.php";
-$sql1= "select id from user where id=\"$_POST[username]\" or e_mail=\"$_POST[email]\"";
-$query = $con->query($sql1);
 
+// read all row from database table
+$sql= "SELECT Class, name, level, exp, win, lose, draw from characterinfo";
+$result = $connection->query($sql);
+
+if(!$result){
+    die("Invalid query: " . $connection->error);
+}
+
+// read data of each row
+while($row = $result->fetch_assoc()){
+    echo "<tr>
+    <th>" . $row["Class"] . "</th>
+    <th>" . $row["name"] . "</th>
+    <th>" . $row["level"] . "</th>
+    <th>" . $row["exp"] . "</th>
+    <th>" . $row["win"] . "</th>
+    <th>" . $row["lose"] . "</th>
+    <th>" . $row["draw"] . "</th>
+</tr>";
+}
 ?>
